c2db3286bff9e53c40d3367c527001921b9761d4
TRENDYOL\ahmetcan.guven <ahmetcanguven44@gmail.com>
2017-11-09 19:28:41 +0300

    Config type selection basics

--- bin/module.js
+++ bin/module.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
-const startArguments = require('minimist')(process.argv.slice(2));
+process.stdout.write("\u001b[2J\u001b[0;0H");
+console.log(require('../lib/theme').introText);
 
+const startArguments = require('minimist')(process.argv.slice(2));
 require('../lib').applyArgument(startArguments);
